274ec6fc96fa72b5493025b3dfdf1a8f9cf36995
gabrielasanchezt <mgabriela.st@gmail.com>
2025-05-08 16:50:25 -0400

    missingo update

--- MLUnsupervisedApp/app.py
+++ MLUnsupervisedApp/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import missingno as msno
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans, AgglomerativeClustering
